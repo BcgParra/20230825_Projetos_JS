2b587a6cfc194ccd7abf80e832002a5190b525a0
BcgParra <138938253+BcgParra@users.noreply.github.com>
2023-09-01 20:57:33 -0300

    Add files via upload

--- 03-slideshow/js/slideShow.js
+++ 03-slideshow/js/slideShow.js
@@ -1,21 +1,21 @@
-
+/*  
 const images = [
-    { 'id': '1', 'url': './img/chrono.jpg' },
-    { 'id': '2', 'url': './img/inuyasha.jpg' },
-    { 'id': '3', 'url': './img/ippo.jpg' },
-    { 'id': '4', 'url': './img/tenchi.jpg' },
-    { 'id': '5', 'url': './img/tenjihotenge.jpg' },
-    { 'id': '6', 'url': './img/yuyuhakusho.jpg' }
+    { 'id': '1', 'url': '../img/chrono.jpg' },
+    { 'id': '2', 'url': '../img/inuyasha.jpg' },
+    { 'id': '3', 'url': '../img/ippo.jpg' },
+    { 'id': '4', 'url': '../img/tenchi.jpg' },
+    { 'id': '5', 'url': '../img/tenjihotenge.jpg' },
+    { 'id': '6', 'url': '../img/yuyuhakusho.jpg' }
 ];
 
 const containerItems = document.querySelector('#container-slide');
 
 const loadImages = (images, container) => {
 
-    images.forEach(image => {
-        container.innerHtml +=
+    images.forEach((image) => {
+        container.innerHTML +=
             `<div class='item'>
-        <img src='${image.url}'>
+        <img src='${image.url}'
         <\div>`
     });
 };
@@ -40,4 +40,44 @@ document.querySelector('#previous').addEventListener('click', anterior);
 document.querySelector('#next').addEventListener('click', proximo);
 
 
+*/
 
+
+const images = [
+    { 'id': '1', 'url':'./img/chrono.jpg' },
+    { 'id': '2', 'url':'./img/inuyasha.jpg' },
+    { 'id': '3', 'url':'./img/tenchi.jpg' },
+    { 'id': '4', 'url':'./img/tenjhotenge.jpg' },
+    { 'id': '5', 'url':'./img/yuyuhakusho.jpg' },
+    { 'id': '6', 'url':'./img/ippo.png' },
+]
+
+const containerItems = document.querySelector('#container-items');
+
+const loadImages = ( images, container ) => {
+    images.forEach ( image => {
+        container.innerHTML += `
+            <div class='item'>
+                <img src='${image.url}'
+            </div>
+        `
+    })
+}
+
+loadImages( images, containerItems );
+
+let items = document.querySelectorAll('.item');
+
+const anterior = () => {
+    containerItems.appendChild(items[0]);
+    items = document.querySelectorAll('.item');
+};
+
+const proximo = () => {
+    const lastItem = items[items.length - 1];
+    containerItems.insertBefore( lastItem, items[0] );
+    items = document.querySelectorAll('.item');
+}
+
+document.querySelector('#previous').addEventListener('click', anterior);
+document.querySelector('#next').addEventListener('click', proximo);
